57178f0d1507ac0289b666d150d1ce0d70dfbfe8
Maxime Vincent <maxime.vincent1@universite-paris-saclay.fr>
2023-02-02 22:16:50 +0100

    Création de fonctions pour la signature avec l'algorithme DSA

--- mini_btc/utils.py
+++ mini_btc/utils.py
@@ -1,7 +1,11 @@
-import socket, json, hashlib, sys
-from base58 import b58encode
-from typing import Callable, Tuple, Union
+import socket, json, sys
 import datetime as dt
+from base58 import b58encode, b58decode
+from binascii import hexlify, unhexlify
+from Crypto.PublicKey import DSA
+from Crypto.Signature import DSS
+from Crypto.Hash import SHA256
+from typing import Callable, Tuple, Union
 
 
 def json_encode(obj: object) -> bytes:
@@ -92,10 +96,43 @@ def sha256(obj: object) -> str:
     :param obj: Objet Python sérialisable en JSON.
     :return: String du hash de l'objet.
     """
-    return hashlib.sha256(json_encode(obj)).hexdigest()
+    return SHA256.new(json_encode(obj)).hexdigest()
 
 
-def rsa_publickey(private_key: object) -> Tuple[str, str]:
+def dsa_generate(size=1024) -> DSA.DsaKey:
+    """
+    Génère une clé privée DSA aléatoire.
+
+    :param size: Taille de la clé.
+    :return: Clé privée secrète.
+    """
+    private_key = DSA.generate(size)
+    return private_key
+
+
+def dsa_export(private_key: DSA.DsaKey, key_file: str):
+    """
+    Exporte une clé privée dans un fichier binaire.
+
+    :param private_key: Clé privée secrète.
+    :param key_file: Chemin du fichier de sortie.
+    """
+    with open(key_file, 'wb') as f:
+        f.write(private_key.export_key("DER"))
+
+
+def dsa_import(key_file: str) -> DSA.DsaKey:
+    """
+    Importe une clé privée depuis un fichier binaire.
+
+    :param key_file: Chemin du fichier binaire.
+    :return: Clé privée secrète.
+    """
+    with open(key_file, 'rb') as f:
+        return DSA.import_key(f.read())
+
+
+def dsa_publickey(private_key: DSA.DsaKey) -> Tuple[str, str]:
     """
     Permet d'obtenir la clé publique et l'adresse à partir de la clé privée.
     Elles sont encodées en base 58.
@@ -103,10 +140,40 @@ def rsa_publickey(private_key: object) -> Tuple[str, str]:
     :param private_key: Clé privée secrète.
     :return: Le couple clé publique, adresse.
     """
-    public_key = private_key.publickey().exportKey("DER")
-    address = b58encode(hashlib.sha256(public_key).digest()).decode('utf-8')
+    public_key = private_key.publickey().export_key("DER")
+    address = b58encode(SHA256.new(public_key).digest()).decode('utf-8')
     public_key = b58encode(public_key).decode('utf-8')
     return public_key, address
+
+
+def dsa_sign(private_key: DSA.DsaKey, data: object) -> str:
+    """
+    Permet de signer une transaction en cryptant son hash avec la clé privée.
+
+    :param private_key: Clé privée secrète.
+    :param data: Données à hacher.
+    :return: Chaîne encryptée du hash.
+    """
+    hash = SHA256.new(json_encode(data))
+    return hexlify(DSS.new(private_key, 'fips-186-3').sign(hash)).decode("utf-8")
+
+
+def dsa_verify(public_key: str, sign: str, data: object) -> bool:
+    """
+    Vérifie la signature selon la clé publique.
+
+    :param public_key: Clé publique pour décrypter la signature.
+    :param sign: Signature à vérifier.
+    :param data: Données à hacher.
+    :return: True si la signature correspond au hash False sinon.
+    """
+    try:
+        public_key = DSA.import_key(b58decode(public_key.encode("utf-8")))
+        hash = SHA256.new(json_encode(data))
+        DSS.new(public_key, 'fips-186-3').verify(hash, unhexlify(sign))
+        return True
+    except:
+        return False
 
 
 def logging(msg: Union[str, object]) -> None:
