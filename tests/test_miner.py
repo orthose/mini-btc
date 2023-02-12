1357a80602d394f21cca760b1fdd16c22a38d19d
Maxime Vincent <maxime.vincent1@universite-paris-saclay.fr>
2023-02-12 23:17:39 +0100

    Correction de test_miner.py et suppression des tests incompatibles avec les transactions UTXO

--- tests/test_miner.py
+++ tests/test_miner.py
@@ -2,36 +2,67 @@ from mini_btc import Miner, Wallet
 from time import sleep
 
 
+# Chargement des porte-feuilles
+alice = Wallet("./wallets/alice.bin", "localhost", 8003, "localhost", 8000)
+# La récompense de minage ira à Alice
+# Donc à chaque bloc miné elle gagne forcément 50 BTC
+alice_pubkey = alice.public_key
+
+bob = Wallet("./wallets/bob.bin", "localhost", 8004, "localhost", 8001)
+bob_pubkey = bob.public_key
+
 # Le premier noeud n'est connecté à aucun autre
-n1 = Miner("localhost", 8000)
+n1 = Miner(alice_pubkey, "localhost", 8000, difficulty=4)
 # Le deuxième noeud est connecté au premier
-n2 = Miner("localhost", 8001, remote_host="localhost", remote_port=8000)
+n2 = Miner(alice_pubkey, "localhost", 8001, remote_host="localhost", remote_port=8000, difficulty=4)
 # Le troisième noeud est aussi connecté au premier
-n3 = Miner("localhost", 8002, remote_host="localhost", remote_port=8000)
+n3 = Miner(alice_pubkey, "localhost", 8002, remote_host="localhost", remote_port=8000, difficulty=4)
 
 # Démarrage des noeuds
-n1.start()
-n2.start()
-sleep(1)
-n3.start()
-sleep(1)
+n1.start(); sleep(1)
+n2.start(); sleep(1)
+n3.start(); sleep(1)
 
-w = Wallet("./wallet.bin", "localhost", 8003, "localhost", 8000)
-w.start()
+# Démarrage des porte-feuilles
+alice.start(); sleep(1)
+bob.start(); sleep(1)
 
 # Génération du bloc de départ
-n1.genesis()
-sleep(1)
+# Pour lancer le minage on envoie des transactions vides
+alice.empty_transfer(); sleep(1)
+alice.empty_transfer(); sleep(10)
 
 n1.logging(n1.ledger)
 n2.logging(n2.ledger)
 n3.logging(n3.ledger)
 
-w.transfer(dest="ykvoezkvoek", amount=1)
-w.transfer(dest="ejiajciajei", amount=2)
-w.transfer(dest="ckkziizhrki", amount=3)
-sleep(40)
+# Vérification du solde de Alice et Bob
+alice.update_balance(); sleep(1)
+assert 50 == alice.get_balance()
 
-n1.logging(n1.ledger)
-n2.logging(n2.ledger)
-n3.logging(n3.ledger)
+bob.update_balance(); sleep(1)
+assert 0 == bob.get_balance()
+
+# Alice envoie 10 BTC à Bob
+alice.transfer(bob_pubkey, 10); sleep(1)
+alice.empty_transfer(); sleep(10)
+
+# Vérification du solde de Alice et Bob
+alice.update_balance(); sleep(1)
+assert (50-10) + 50 == alice.get_balance()
+
+bob.update_balance(); sleep(1)
+assert 10 == bob.get_balance()
+
+# Alice envoie 5 BTC à Bob
+alice.transfer(bob_pubkey, 5); sleep(1)
+# Bob envoie 3 BTC à Alice
+bob.transfer(alice_pubkey, 3)
+sleep(10)
+
+# Vérification du solde de Alice et Bob
+alice.update_balance(); sleep(1)
+assert (50-10) + (50-5) + (50+3) == alice.get_balance()
+
+bob.update_balance(); sleep(1)
+assert 10-3+5 == bob.get_balance()
